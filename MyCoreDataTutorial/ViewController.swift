52e1f63d5ad17896bd95b107e3c252bb37962b2f
Joakim Sjöstedt <joakim.sjostedt@softhouse.se>
2020-02-28 12:34:04 +0100

    Initial Commit

---
+++ MyCoreDataTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCoreDataTutorial
+//
+//  Created by Joakim Sjöstedt on 2020-02-28.
+//  Copyright © 2020 Joakim Sjöstedt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
